447758d721241aa702e16648dfc90f241db2102f
Jimale <jimale@tiriig.com>
2024-05-05 17:31:19 +0300

    Added ImageView

--- app/src/main/java/com/atustudentsafternoon/MainActivity.kt
+++ app/src/main/java/com/atustudentsafternoon/MainActivity.kt
@@ -4,9 +4,11 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -18,7 +20,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         openNewPage()
+        changeImage()
         //displayStudentName()
+    }
+
+    private fun changeImage(){
+        val imageView: ImageView = findViewById(R.id.student_img)
+        val button: Button = findViewById(R.id.display_btn)
+
+        button.setOnClickListener {
+            imageView.setImageResource(R.drawable.image_two)
+        }
     }
 
     private fun openNewPage() {
