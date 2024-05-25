eba6e511e9fd6964f048db02028b96948943d0ec
Jimale <jimale@tiriig.com>
2024-05-25 11:05:09 +0300

    Added comments

--- app/src/main/java/com/atustudentsafternoon/DatabaseActivityActivity.kt
+++ app/src/main/java/com/atustudentsafternoon/DatabaseActivityActivity.kt
@@ -38,6 +38,7 @@ class DatabaseActivityActivity : AppCompatActivity() {
         val studentEmail: EditText = findViewById(R.id.student_email_et)
         val studentAge: EditText = findViewById(R.id.student_age_et)
 
+        //Student entity or student table
         val student = Student(
             id = 1,
             name = studentName.text.toString(),
@@ -52,15 +53,21 @@ class DatabaseActivityActivity : AppCompatActivity() {
 
     private fun displayStudent(){
         Thread{
+
             val database = Room.databaseBuilder(
                 applicationContext,
                 MyDatabase::class.java, "my_db"
             ).build()
 
+            //Declare views
             val textView: TextView = findViewById(R.id.display_student_tv)
+
+            //Get the students from the database
             val students = database.studentDao().getStudents()
 
+            //Get back to MAIN UI thread to display student info
             runOnUiThread {
+                //Display student
                 textView.text = students.toString()
             }
 
